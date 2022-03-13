0bc1ad1e015cad850d96e453cc340aed19b86e9c
crono628 <84817352+crono628@users.noreply.github.com>
2022-03-13 18:54:46 -0400

    css update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const [currentForecast, setCurrentForecast] = useState([]);
   const [sevenDay, setSevenDay] = useState([]);
   const [zip, setZip] = useState(
-    Math.floor(parseInt(zips[Math.floor(Math.random() * zips.length)]))
+    Math.floor(parseInt(zips[Math.floor(Math.random() * zips.length)]) || null)
   );
   const [icon, setIcon] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -90,49 +90,54 @@ const App = () => {
         handleSubmit={handleSubmit}
         onClick={handleSubmit}
       />
-      <Routes>
-        <Route
-          path="/react-weather-app/"
-          element={
-            <Home
-              date={date.toLocaleDateString()}
-              city={loading ? null : currentForecast.name}
-              src={loading ? null : icon}
-              temp={loading ? null : Math.floor(currentForecast.main.temp)}
-              state={loading ? null : geo[0].state}
-              boolSwitch={loading}
-              desc={loading ? null : currentForecast.weather[0].description}
-            />
-          }
-        />
-        <Route
-          path="/current"
-          element={
-            <Current
-              date={date.toLocaleDateString()}
-              city={loading ? null : currentForecast.name}
-              src={loading ? null : icon}
-              temp={loading ? null : Math.floor(currentForecast.main.temp)}
-              state={loading ? null : geo[0].state}
-              boolSwitch={loading}
-              desc={loading ? null : currentForecast.weather[0].description}
-              hourly={
-                loading
-                  ? null
-                  : hourlyTemps.map((item) => {
-                      return (
-                        <div key={uniqid()}>
-                          <div className="hourly-time">{`${item.time.getHours()}:00`}</div>
-                          <div className="hourly-temp">{item.temp}°</div>
-                        </div>
-                      );
-                    })
-              }
-            />
-          }
-        />
-        <Route path="/seven-day" element={<SevenDay />} />
-      </Routes>
+      <div class="interface">
+        <Routes>
+          <Route
+            path="/react-weather-app/"
+            element={
+              <Home
+                date={date.toLocaleDateString()}
+                city={loading ? null : currentForecast.name}
+                src={loading ? null : icon}
+                temp={loading ? null : Math.floor(currentForecast.main.temp)}
+                state={loading ? null : geo[0].state}
+                boolSwitch={loading}
+                desc={loading ? null : currentForecast.weather[0].description}
+              />
+            }
+          />
+          <Route
+            path="/current"
+            element={
+              <Current
+                date={date.toLocaleDateString()}
+                city={loading ? null : currentForecast.name}
+                src={loading ? null : icon}
+                temp={loading ? null : Math.floor(currentForecast.main.temp)}
+                state={loading ? null : geo[0].state}
+                boolSwitch={loading}
+                desc={loading ? null : currentForecast.weather[0].description}
+                hourly={
+                  loading
+                    ? null
+                    : hourlyTemps.map((item) => {
+                        return (
+                          <div key={uniqid()}>
+                            <div className="hourly-date">{`${
+                              item.time.getMonth() + 1
+                            }/${item.time.getDate()}`}</div>
+                            <div className="hourly-time">{`${item.time.getHours()}:00`}</div>
+                            <div className="hourly-temp">{item.temp}°</div>
+                          </div>
+                        );
+                      })
+                }
+              />
+            }
+          />
+          <Route path="/seven-day" element={<SevenDay />} />
+        </Routes>
+      </div>
     </div>
   );
 };
